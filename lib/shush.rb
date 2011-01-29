925fc68e245f6c3cb1f516f7427650c5a43d9748
Mitchell Amihod <mitchell.amihod@gmail.com>
2011-01-29 08:40:22 -0500

    switch to argv for ddelete

--- lib/shush.rb
+++ lib/shush.rb
@@ -3,9 +3,8 @@ require 'mechanize'
 
 require '~/.github-shush-config.rb'
 
-config = {
-  :delete => false
-}
+# either mark as read, or delete
+action = (ARGV.to_s == 'delete') ? "delete" : "get"
 
 github = {
   :login => 'https://github.com/login',
@@ -16,6 +15,7 @@ github = {
 # we may have some issues ie: when its the same message x2 or whatnot - 
 # github doesnt send enough of identifier to know which notification it is.
 # so, we will only focus on unread messages, and then try to match the title/ text
+# this is a hint - the original plan was this would receive clicks from gmail
 # message_subject = nil
 # message_text = nil
 
@@ -23,6 +23,7 @@ a = Mechanize.new { |agent|
   agent.user_agent_alias = 'Mac Safari'
 }
 
+puts "Logging in as #{$github_config[:username]}"
 a.get(github[:login]) do |page|
   login_result = page.form_with(:action => '/session') do |login_form|
     login_form.login = $github_config[:username]
@@ -30,9 +31,8 @@ a.get(github[:login]) do |page|
   end.submit
 end
 
-# either mark as read, or delete
-action = config[:delete] ? "delete" : "get"
 a.get(github[:notifications]).search(".unread a.body").each do |link|
+  puts "doing  #{link.text}"
   url = "https://github.com"+link.get_attribute('href')
   a.send(action, url)
 end
